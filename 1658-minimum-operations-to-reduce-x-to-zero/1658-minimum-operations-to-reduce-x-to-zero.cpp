3c0bdf50fa375486e98876b03b8d67a0fdb0a027
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-06-12 02:55:51 +0530

    Time: 273 ms (60.15%), Space: 98.5 MB (91.02%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) 
+{    int start =0;
+        int end =0;
+        int totalsum = accumulate(nums.begin(),nums.end(),0); 
+        int sum_to_make = totalsum - x; 
+        if(totalsum==x)return nums.size();
+        int ans = 0;
+        int sum =0;
+        
+        while(end<nums.size())
+        {  sum+=nums[end]; 
+            
+         while(start<end && sum>sum_to_make) 
+         {
+             sum-=nums[start++]; 
+         }
+         if(sum==sum_to_make) 
+         {
+             ans = max(ans,end-start+1); 
+         }
+         
+           end++;
+            
+        }
+        
+        return ans==0 ? -1 : nums.size()-ans;}
+};
