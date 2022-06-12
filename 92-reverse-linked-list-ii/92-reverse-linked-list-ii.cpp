cb948913b028557e23de3499bba349e17602accd
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-06-13 03:47:13 +0530

    Time: 7 ms (17.16%), Space: 7.4 MB (92.76%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,19 +10,57 @@
  */
 class Solution {
 public:
-   ListNode* reverseBetween(ListNode* head, int m, int n) {
-       ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
-       dummy -> next = head;
-       for (int i = 0; i < m - 1; i++) {
-           pre = pre -> next;
-       }
-       cur = pre -> next;
-       for (int i = 0; i < n - m; i++) {
-           ListNode* temp = pre -> next;
-           pre -> next = cur -> next;
-           cur -> next = cur -> next -> next;
-           pre -> next -> next = temp;
-       }
-       return dummy -> next;
-   }
+    ListNode* reverseBetween(ListNode* head, int left, int right) 
+    {  ListNode* current = head, *prev = NULL;
+        int count = 1;
+         while(count != left){
+             prev = current;
+             current = current->next;
+             count++;
+         }
+        
+         ListNode* start = current;
+         while(count != right){
+            current = current->next;
+            count++;
+         }
+       
+         ListNode* rest = current->next;
+         current->next = NULL;
+        
+         ListNode* newHead = reverse(start);
+         if (prev!=NULL){
+             prev->next = newHead;
+
+        }
+        current = newHead;
+         while(current->next!=NULL){
+             current= current->next;
+         }
+         current->next = rest;
+         if(left == 1){
+             return newHead;
+         }
+         else{
+            return head;
+        }
+        
+        
+    }
+    ListNode* reverse(ListNode* head){
+        
+        ListNode* prev = NULL, *next = NULL, *current = head;
+        while(current != NULL){
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+            
+        }
+        
+        return prev;
+    }
+
+        
+    
 };
