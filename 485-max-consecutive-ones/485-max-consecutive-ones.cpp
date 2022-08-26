dbdcd40a44932d11513966896d059e2964a9c7ab
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-08-27 01:20:03 +0530

    Time: 79 ms (19.42%), Space: 36.1 MB (95.36%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -1,33 +1,46 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int n=nums.size();
-        int mx=0;
-        int i=0,j=1;
-        while(j<n)
-        {
-            if(nums[i]==1&&nums[j]==1)
-            {
-                 j++;
-            }
-          else if(nums[i]==1&&nums[j]==0)
-          {
-               mx=std::max(mx,j-i);
+//         int n=nums.size();
+//         int mx=0;
+//         int i=0,j=1;
+//         while(j<n)
+//         {
+//             if(nums[i]==1&&nums[j]==1)
+//             {
+//                  j++;
+//             }
+//           else if(nums[i]==1&&nums[j]==0)
+//           {
+//                mx=std::max(mx,j-i);
               
-              i=j;j++;
-          }
-            else{
-                  i++;j++;
-                }
+//               i=j;j++;
+//           }
+//             else{
+//                   i++;j++;
+//                 }
                 
-         }
+//          }
         
-           if(nums[i]==1&&nums[n-1]==1)
-          {
-               mx=std::max(mx,n-i);
-           }
+//            if(nums[i]==1&&nums[n-1]==1)
+//           {
+//                mx=std::max(mx,n-i);
+//            }
         
-        return mx;
+//         return mx;
+        int count =0;
+    int maxi = INT_MIN;
+    for(int i =0 ; i<nums.size(); i++){
+        if(nums[i]==1){
+            count++;
+        }
+        if(nums[i]!=1){
+            count=0;
+        }
+        maxi = max(count , maxi);
+    }
+    
+    return maxi;
         
         
         
