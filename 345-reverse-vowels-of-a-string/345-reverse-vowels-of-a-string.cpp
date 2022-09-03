ea5a1aebe4596f7bb9aea7397d8af404f4be937c
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-09-03 16:53:35 +0530

    Time: 12 ms (47.99%), Space: 7.8 MB (40.94%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -1,37 +1,37 @@
-class Solution {
-     bool isVowel(string &str,int indx)
-{
-    if(str.at(indx) == 'a' || str.at(indx) == 'e' || str.at(indx) == 'i' || str.at(indx) == 'o' || str.at(indx) == 'u' || str.at(indx) == 'A' || str.at(indx) == 'E' || str.at(indx) == 'I' || str.at(indx) == 'O' || str.at(indx) == 'U')
-    {
-     return true;
-    }
-    return false;
-}
-    string reverseVowelss(string &s)
-{
-    int left = 0;
-    int right = s.size()-1;
+// class Solution {
+//      bool isVowel(string &str,int indx)
+// {
+//     if(str.at(indx) == 'a' || str.at(indx) == 'e' || str.at(indx) == 'i' || str.at(indx) == 'o' || str.at(indx) == 'u' || str.at(indx) == 'A' || str.at(indx) == 'E' || str.at(indx) == 'I' || str.at(indx) == 'O' || str.at(indx) == 'U')
+//     {
+//      return true;
+//     }
+//     return false;
+// }
+//     string reverseVowelss(string &s)
+// {
+//     int left = 0;
+//     int right = s.size()-1;
    
-    while(left<right)
-    {
-        while(left<right && isVowel(s,left) == false)
-        {
-            left++;
-        }
-        while(left<right && isVowel(s,right) == false)
-        {
-            right--;
-        }
+//     while(left<right)
+//     {
+//         while(left<right && isVowel(s,left) == false)
+//         {
+//             left++;
+//         }
+//         while(left<right && isVowel(s,right) == false)
+//         {
+//             right--;
+//         }
        
-        swap(s.at(left),s.at(right));
-        left++;
-        right--;
-    }
-    return s;
-}
+//         swap(s.at(left),s.at(right));
+//         left++;
+//         right--;
+//     }
+//     return s;
+// }
 
-public: string reverseVowels(string s) {
- return reverseVowelss(s);     
+// public: string reverseVowels(string s) {
+//  return reverseVowelss(s);     
 
 
 
@@ -41,4 +41,21 @@ public: string reverseVowels(string s) {
     
     
     
-}};
+// }};
+
+
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            i = s.find_first_of("aeiouAEIOU", i);
+            j = s.find_last_of("aeiouAEIOU", j);
+            if (i < j) {
+                swap(s[i++], s[j--]);
+            }
+        }
+        return s;
+    }
+};
