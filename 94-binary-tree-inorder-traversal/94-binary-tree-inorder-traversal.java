11e54ee35eaf51b3f11bd146a62d0b85fd6088ad
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-09-17 22:17:10 +0530

    Time: 2 ms (47.23%), Space: 42.2 MB (49.04%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -14,13 +14,8 @@
  * }
  */
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) 
-         {
-        List<Integer> ans =new ArrayList<>();
-        traversal(root,ans);
-        return ans;
-    }
-        // Helping function : 
+   
+            // Helping function recursive: 
         public void traversal(TreeNode root,List<Integer> ans)
         {
             
@@ -31,5 +26,40 @@ class Solution {
             traversal(root.right,ans);
             
         }
+    public List<Integer> inorderTraversal(TreeNode root) 
+         {
+        List<Integer> ans =new ArrayList<>();
+  //for recursive next 2 lines
+        // traversal(root,ans);
+        //return ans;
+        
+        //ITERATIVE CODE
+
+            Stack<TreeNode>st=new Stack<>();
+        while(true)
+        {
+            if(root!=null)
+            {
+                st.push(root);
+                root=root.left;
+            }
+            else{
+                if(st.size()==0)
+                {
+                    break;
+                }
+            root=st.pop();
+                ans.add(root.val);
+                root=root.right;
+                
+            }
+            
+    }
+    return ans;    
+        
+        
+        
+    }
+
         
 }
