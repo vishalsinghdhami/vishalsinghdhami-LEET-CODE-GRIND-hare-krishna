510119c8f032eba5e2bb4b4aaa4dc96221c4f91f
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-09-28 02:31:05 +0530

    Time: 33 ms (54.44%), Space: 19.6 MB (59.65%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,28 +1,58 @@
 class Solution {
 public:
-   //Hash Table:
-    int majorityElement(vector<int>& nums) {
-      
-        int n=nums.size();
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++)
-        {
-            m[nums[i]]++;
-        }
-        int ans;
-        for(auto x: m)
-        {
-            if(x.second>n/2)
-            {
-                ans= x.first;
-            }
-        }
-        
-    return ans;
+    //   MOORES VOOTING ALGO
     
-    //Sorting
-// int majorityElement(vector<int>& nums) {
-        
-//    
+     //OPTIMISED O(N) time  O(1) SPACE
+ int majorityElement(vector<int>& nums) {
+
+     int freq=0;
+     int ans=0;
+     for(int i=0;i<nums.size();i++)
+     {
+         if(freq==0)
+         {
+             ans=nums[i];
+         }
+         
+         if(nums[i]==ans)
+         {
+             freq++;
+         }
+          else
+             {
+                 freq--;
+             }
+         
+         
+     }
+     
+      return ans;
+    
     }
+    
+    
+    
+    //Hash Table:O(N) time  O(N) SPACE
+//     int majorityElement(vector<int>& nums) {
+      
+//         int n=nums.size();
+//         unordered_map<int,int> m;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             m[nums[i]]++;
+//         }
+//         int ans;
+//         for(auto x: m)
+//         {
+//             if(x.second>n/2)
+//             {
+//                 ans= x.first;
+//             }
+//         }
+        
+//        return ans;
+//     }
+    
+    
+ 
 };
