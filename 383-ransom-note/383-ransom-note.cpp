e473973db65aed6ec27a11aa238d098629bd9c80
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-09-27 13:34:29 +0530

    Time: 56 ms (9.11%), Space: 8.8 MB (40.09%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -3,11 +3,14 @@ public:
     bool canConstruct(string r, string m) {
      //R BNANA HAE M SAE
         unordered_map<char,int> store;
+     //storing  ransom chars in map 
         for(int i=0;i<r.size();i++)
         {
             store[r[i]]++;
          
         }
+      //substracting magazine chars
+       // logic: if all chars of ransom will be present in magazine then all inputs of map will become negative zero...else some indexes value will remain positive 
         for(int i=0;i<m.size();i++)
         {
             
@@ -22,8 +25,5 @@ for(auto x:store)
     
 }
 return true;
-            
-        
-        
-    }
+}
 };
