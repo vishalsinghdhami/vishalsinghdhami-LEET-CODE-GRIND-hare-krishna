b9679fd7de2cab597969e9f152304ffb65d18d60
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-02 01:19:07 +0530

    Time: 127 ms (5.20%), Space: 27.7 MB (12.14%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,35 +1,57 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int t) 
-    { 
-       int mid;
-     int low=0; int high=arr.size()-1;
-       while(high-low>1)
-       {
-          // if(t==arr[mid]){return 1;}
-          mid = (high + low) / 2;
-           if(t<=arr[mid])
+    int search(vector<int>& arr, int target) {
+        //RECURSIVE
+        
+        int n=arr.size();
+        
+        int low=0;
+        int high=n-1;
+        int mid=(low+high)/2;
+        
+        while(high-low>1)
+        {
+           if(arr[mid]>=target)
            {
-               high= mid;
+               if(arr[mid]==target)
+               {
+                   return mid;
+                   break;
+               }
+               high=mid;
+               mid=(low+high)/2;
            }
-           else{
-               low=mid+1;
-               
-           }
-           
-       }
-     
-     if(t==arr[low]){
-         return low;
-     }
-     else if(t==arr[high])
-     { return high;}
-     else{
-         return -1;
-     }
+            else{
+                low=mid;
+                mid=(low+high)/2;
+            }
             
             
-            
-            
+        }
+        
+        if(arr[mid]==target)
+        {
+            return mid;
+        }
+        else if(arr[low]==target)
+        {
+            return low;
+        }
+        else if(arr[high]==target)
+        {
+            return high;
+        }
+        
+        
+        else{
+            return -1;
+        }
+        
+        
+        
+        //ITERATIVE
+        
+        
+        
     }
 };
