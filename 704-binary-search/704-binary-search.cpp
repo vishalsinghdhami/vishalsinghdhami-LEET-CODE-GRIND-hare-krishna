d1fab3f13aa42fffe8d45fb4dd5bb99e7e3dd310
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-02 01:52:52 +0530

    Time: 128 ms (5.20%), Space: 42.8 MB (12.10%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,33 +1,67 @@
 class Solution {
-public:
-    int search(vector<int>& arr, int target) {
-        //RECURSIVE
-        
-        
-        //ITERATIVE
-        
-        
-        int n=arr.size();
-        
-        int low=0;
-        int high=n-1;
-      while(low<=high)
-      {
-        int mid=(low+high)/2;
-        
+
+    //RECURSIVE
+     int binarysearch(vector<int> arr,int low,int high,int target)
+        {
+          if(low>high)
+          {
+              return -1;
+          }
+           
+         int mid=(low+high)/2; 
+           
           if(arr[mid]==target)
           {
               return mid;
           }
-          else if(arr[mid]>target)
+          else if (arr[mid]>target)
           {
-              high=mid-1;
+             return binarysearch(arr,low,mid-1,target);
           }
-          else{
-              low=mid+1;
+          else
+          {
+            return binarysearch(arr,mid+1,high,target);
           }
           
-      } 
-        return -1;
+          
+        }       
+    
+    public:
+    int search(vector<int>& arr, int target) 
+    {
+        
+        int n=arr.size();
+        int low=0;int high=n-1;
+       return binarysearch(arr,low,high,target);
+        
     }
-};
+      
+};       
+        
+//          //ITERATIVE
+//     int search(vector<int>& arr, int target) {    
+        
+//         int n=arr.size();
+        
+//         int low=0;
+//         int high=n-1;
+//       while(low<=high)
+//       {
+//         int mid=(low+high)/2;
+        
+//           if(arr[mid]==target)
+//           {
+//               return mid;
+//           }
+//           else if(arr[mid]>target)
+//           {
+//               high=mid-1;
+//           }
+//           else{
+//               low=mid+1;
+//           }
+          
+//       } 
+//         return -1;
+//     }
+// };
