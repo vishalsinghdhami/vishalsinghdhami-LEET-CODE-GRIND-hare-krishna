1597caba643fa9ba58175e325ab1d6884bd3cdb3
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-02 04:02:27 +0530

    Time: 7 ms (16.37%), Space: 6.5 MB (88.90%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int ans = 0;
-        for(int i=s.size()-1;i>=0;--i){
-            if(s[i] == ' ' && ans > 0) return ans;
-            if(s[i] != ' ') ans++;
+        int x=0;
+        int i= s.size()-1;
+        for(int j=s.size()-1;j>0;j--)
+        {
+         if(isspace(s[j]))
+         {
+             i--;
+         }
+            else{
+                break;
+            }
         }
-        return ans;
-    }
+      //  return i;
+       
 
+        while(s[i]!=' '&&i>0)
+        {
+            x++;
+            i--;
+        }
+        if(i==0&&s[i]!=' '){
+         x=x+1;
+        }
+        return x;
+        
+        
+    }
 };
