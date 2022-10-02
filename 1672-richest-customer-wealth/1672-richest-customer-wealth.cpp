c59fb6fa7cd4a4aa396e05c1efbad1e3e4410d09
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-02 14:36:35 +0530

    Time: 13 ms (25.02%), Space: 7.8 MB (87.25%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -4,19 +4,25 @@ public:
         
         int r= accounts.size();
         int c= accounts[0].size();
-        vector<int>v;
+       // vector<int>v;
+        int y=0;
         for(int i=0;i<r;i++)
         {
             int x=0;
+           
             for(int j=0;j<c;j++)
             {
                 x+=accounts[i][j];
             }
             
-            v.push_back(x);
+            //v.push_back(x);
+            
+            if(x>y){
+                y=x;
+            }
         }
         
-          return(*max_element(v.begin(), v.end()));      
-        
+         // return(*max_element(v.begin(), v.end()));      
+        return y;
     }
 };
