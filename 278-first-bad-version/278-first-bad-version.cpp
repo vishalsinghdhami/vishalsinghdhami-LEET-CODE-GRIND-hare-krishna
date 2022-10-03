7f980c69eeb81ae2b50c0ac33e02ee0e9dd58b65
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-03 21:40:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (22.68%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,32 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) 
+    {
+        int low=1;
+        int high= n;
+        vector<int>v;
+        
+        while(low<=high)
+        {
+            int mid=low + (high - low) / 2;;
+            
+            if(isBadVersion(mid))
+            {
+                v.push_back(mid);
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+            
+        }
+        
+        return *min_element(v.begin(), v.end());
+        
+        
+        
+    }
+};
