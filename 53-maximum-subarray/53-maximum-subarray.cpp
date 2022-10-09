49f08e09f907f37174a0cffc8b8d72a64a6d5b9e
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-09 22:31:07 +0530

    Time: 183 ms (58.77%), Space: 74 MB (6.22%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,20 +1,52 @@
-class Solution {
-public:
-    int maxSubArray(vector<int>& a) {
-            int size=a.size();
-  int max_so_far = INT_MIN, max_ending_here = 0;
+// //Kadanes Algorithm
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& a) {
+//             int size=a.size();
+//   int max_so_far = INT_MIN, max_ending_here = 0;
  
-    for (int i = 0; i < size; i++) 
-    {
-        max_ending_here = max_ending_here + a[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
+//     for (int i = 0; i < size; i++) 
+//     {
+//         max_ending_here = max_ending_here + a[i];
+//         if (max_so_far < max_ending_here)
+//             max_so_far = max_ending_here;
  
-        if (max_ending_here < 0)
-            max_ending_here = 0;
-    }
-    return max_so_far;
+//         if (max_ending_here < 0)
+//             max_ending_here = 0;
+//     }
+//     return max_so_far;
     
         
+//     }
+// };
+
+//Efficient way
+class Solution {
+
+public:
+
+	int globalMaxSum;
+
+	int maxSubArray(vector<int> &nums, int n) {
+
+		if (n == 1) return nums[0];
+
+		int currMaxSum = max(nums[n - 1], maxSubArray(nums, n - 1) + nums[n - 1]);
+
+		globalMaxSum = max(globalMaxSum, currMaxSum);
+
+		return currMaxSum;
+
+	}
+
+    int maxSubArray(vector<int> &nums) {
+
+        globalMaxSum = nums[0];
+
+		maxSubArray(nums, nums.size());
+
+        return globalMaxSum;
+
     }
+
 };
