31ea5d90d41ad76dd2e027c4711fcfa484961b1d
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-11 03:15:59 +0530

    Time: 7 ms (64.40%), Space: 9.5 MB (20.94%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& str) {
+        int n = str.size();
+        if(n==0) return "";
+        
+        string ans  = "";
+       //hint :  agr string sort krdi tou sbe chto string aaegi aur sbsi bdi sbse piche tou esliye 1st ko last mae jo common hoga offcourse bich vli mae bhi common hoga
+        sort(str.begin(),str.end());
+        
+        string a = str[0];
+        string b = str[n-1];
+        
+        for(int i=0; i<a.size(); i++){
+            if(a[i]==b[i]){
+                ans = ans + a[i];
+            }
+            else{
+                break;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
