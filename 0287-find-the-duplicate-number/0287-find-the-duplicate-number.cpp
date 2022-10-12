d2bb13998d5b3e086cf2d4067c5cba0a95e040c9
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-12 18:26:43 +0530

    Time: 256 ms (44.68%), Space: 61.2 MB (91.61%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) 
-    {  int ans=0;
-        for( int i=0;i<nums.size();i++)
-        {
-           if(nums[abs(nums[i])-1]<0)
-           { ans=(abs(nums[i]));
-             return ans;
-            break;
-           }
-           
+//     int findDuplicate(vector<int>& nums) 
+//     {  int ans=0;
+//         for( int i=0;i<nums.size();i++)
+//         {
+//            if(nums[abs(nums[i])-1]<0)
+//            { ans=(abs(nums[i]));
+//              return ans;
+//             break;
+//            }
+//           nums[abs(nums[i])-1]= -1*nums[abs(nums[i])-1];
             
-            nums[abs(nums[i])-1]= -1*nums[abs(nums[i])-1];
-            
-            
-        }
+//         }
 
+       int findDuplicate(vector<int>& nums) {
+        // Each index is taken 1 based, as if it is zero based, then
+        // for nums[2] = 3, if we goto nums[nums[2] - 1], it is again nums[2]
+        // infinite loop
+        int hare = nums[0], tortoise = nums[0];
         
+        do {
+            hare = nums[nums[hare]];
+            tortoise = nums[tortoise];
+        } while(hare != tortoise);
         
+        // to find the starting of cycle, make tortoise to start from begining
+        tortoise = nums[0];
+        while(hare != tortoise) {
+            hare = nums[hare];
+            tortoise = nums[tortoise];
+        }
+        
+        return hare;
+    
+       }
         
         //    int len = nums.size();
         // for (int i = 0; i < len; ) {
@@ -32,16 +48,8 @@ public:
         //     }
         //}
 
-        return 0;
-        
-        
-        
-        
-        
-        
-        
-        
-}
+        //return 0;
+    //}
         
         
         
