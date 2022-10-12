127329799fc4bdf213c0c21f42f704288397cff6
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-12 17:40:36 +0530

    Time: 56 ms (65.38%), Space: 22 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,14 +1,14 @@
 //  LOGIC: sum of 2 sides greater then 3rd side
 class Solution {
-    bool check(int i,int j,vector<int> nums)
-    {
-        if(nums[i]+nums[j]>nums[j+1]&&nums[j]+nums[j+1]>nums[i]&&nums[j+1]+nums[i]>nums[j])
-        {
-            return true;
-        }
-        else{
-               return false;    }
-         }
+    // bool check(int i,int j,vector<int> nums)
+    // {
+    //     if(nums[i]+nums[j]>nums[j+1])//&&nums[j]+nums[j+1]>nums[i]&&nums[j+1]+nums[i]>nums[j])
+    //     {                                  //as array is alread sorted we dont wanna check other then 1st check it will come true if 1st qualify
+    //         return true;
+    //     }
+    //     else{
+    //            return false;    }
+    //      }
 public:
     //noob way 
     int largestPerimeter(vector<int>& nums) {
@@ -19,10 +19,10 @@ public:
         int j=n-2;
        
         while(i>=0)
-        {  bool checkvar=check(i,j,nums);
-         if(checkvar==true)
-         {  int sum=nums[i]+nums[j]+nums[j+1];
-             return sum;
+        {  
+         if(nums[i]+nums[j]>nums[j+1])
+         {  return nums[i]+nums[j]+nums[j+1];
+            
           break;
          }
          else{
