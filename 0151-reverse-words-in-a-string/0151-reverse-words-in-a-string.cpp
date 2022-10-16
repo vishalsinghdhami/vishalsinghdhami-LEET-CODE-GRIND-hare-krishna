ee66f650cc000d5d800a94774d3f721387ef62cf
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-16 10:53:20 +0530

    Time: 19 ms (30.08%), Space: 6.9 MB (96.74%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,55 +1,47 @@
-//bekar solution : string vector  mae push kra dae phir reverse krke ae string add rkte krte ans aajaega
-
-//niche hae bdiya solution
-// O (1) space vla
 class Solution {
 public:
-  
-   void rev(string &s,int l,int r){
-        while(l<r)
-            swap(s[l++],s[r--]);
-    }
     string reverseWords(string s) {
-        int i,j,n=s.length();
-        i=0;
-        
-        rev(s,0,n-1);
-        while(i<n){
-            
-            // Remove leading spaces
-            j=i;
-            while(j<n && s[j]==' ')
-                j++;
-            
-            int cnt=0;
-            
-            while(j<n && s[j]!=' '){
-                j++;
-                cnt++;
+        //1//step //Remove spaces from the beginning and end
+        while(s[0] == ' ') {s.erase(0, 1);} 
+        while(s[s.size()-1]==' ') {s.erase(s.size()-1);} 
+     
+        //2//step//count no of spaces as we have to give only one space b/w words in ans string
+          int i = 0, space_count = 0;
+
+        while(s[i])
+        {
+		
+		   /*if the character is not space, then this is the starting of a new word. 
+		   So, make the space count 0.*/
+		   
+            if(s[i] == ' ') space_count++;
+            else space_count = 0; 
+            if(space_count > 1) //more than one space found
+            {
+                s.erase(i,1);
+                continue;
             }
-            
-            if(j>n)
-                break;
-            j--;
-            
-            if(i==0) 
-            rev(s,i,j);
-            else     rev(s,i+1,j);
-            
-            if(cnt==0)
-                i=j+1;
-            else
-                i=i+cnt;
+            i++;
+        }
+        //3// reverse complete string
+         reverse(s.begin(),s.end());
+        
+        //4//reverse individual words  // space found means we have a word in our left and we have to reverse it
+        int left = 0;
+        for(i=1;i<s.size();i++)
+        {
+            if(s[i] == ' ') 
+            {
+                reverse(s.begin()+left, s.begin()+i);
+                left = i+1;
+            }
+            else if(i == s.size()-1)
+            {
+                reverse(s.begin()+left, s.begin()+i+1);
+            }
         }
         
-        j=n-1;
-        while(j>=0 && s[j]==' '){
-            j--;
-            s.pop_back();
-        }
         return s;
         
     }
-        
-    
 };
