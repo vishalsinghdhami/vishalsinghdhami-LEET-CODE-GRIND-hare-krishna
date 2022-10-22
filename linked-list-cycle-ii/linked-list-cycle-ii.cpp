8b8590fee3984eadf371dcd893f90b5639723d13
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-22 19:24:18 +0530

    Time: 3 ms, Memory: 7.6 MB - LeetHub

---
+++ linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,30 @@
+//wehave to return the postion of cycle  connecting pt;
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+                if(head==NULL||head->next==NULL){return NULL;}
+        
+        ListNode*slow=head,*fast=head;
+   
+        while(fast!=NULL&&fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                break;
+            }
+            
+        }
+        if (!(fast && fast->next)) return NULL;
+        while(head!=slow)
+        {
+            head=head->next;
+            slow=slow->next;
+        }
+        
+       return head;
+        
+        
+    }
+};
