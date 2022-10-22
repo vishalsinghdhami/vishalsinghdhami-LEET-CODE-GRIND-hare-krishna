b9533f48ca93a7698bc8a969bf91bb80e0acb26e
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-10-23 02:03:07 +0530

    Time: 83 ms (16.14%), Space: 71.5 MB (12.19%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -5,7 +5,7 @@ public:
         ListNode *curr = dummy;
         int carry = 0;
         
-        while(l1 != NULL || l2 != NULL || carry == 1){
+        while(l1 != NULL || l2 != NULL || carry){
             int sum = 0;
             if(l1 != NULL){
                 sum += l1->val;
