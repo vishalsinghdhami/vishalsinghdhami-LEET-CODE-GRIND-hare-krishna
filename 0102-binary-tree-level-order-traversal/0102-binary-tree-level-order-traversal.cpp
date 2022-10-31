e0a360e92ee83b60972f02cf7485c4c64a14e18b
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-11-01 01:03:41 +0530

    Time: 4 ms (80.54%), Space: 12.7 MB (32.16%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -6,26 +6,27 @@ public:
         queue<TreeNode*> q;
         q.push(root);
         q.push(NULL);
-       
-        while(!q.empty()) 
-        {   vector<int> cur_vec;
+       vector<int> cur_vec;
+       // while(!q.empty()) 
+      //  {   vector<int> cur_vec;
             while(!q.empty())
         {
             TreeNode* t = q.front();
             q.pop();
             if (t==NULL) {
                 result.push_back(cur_vec);
-                //cur_vec.resize(0);
+                cur_vec.resize(0);
                 if (q.size() > 0) {
                     q.push(NULL);
                 }
-                break;
+                //break;
             } else {
                 cur_vec.push_back(t->val);
                 if (t->left) q.push(t->left);
                 if (t->right) q.push(t->right);
             }
-        }}
+        }
+    //}
         return result;
     }
 };
