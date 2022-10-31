bf95a6e376282460f1aad926205a3a853a7206fe
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-11-01 01:44:52 +0530

    Time: 35 ms (26.51%), Space: 22.5 MB (71.38%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.cpp
@@ -1,41 +1,59 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// class Solution {
+// public:
+//     vector<double> averageOfLevels(TreeNode* root) {
+//         vector<double>result;
+//         if (!root) return result;
+//         queue<TreeNode*>q;
+//    q.push(root);
+//    q.push(NULL);
+//    double sum=0,x=0;
+//         while(!q.empty())
+//         {  TreeNode* first = q.front();
+//             if(first==NULL)
+//             { q.pop();
+//               result.push_back(sum/x);
+//               x=0;sum=0; 
+//            if (q.size() > 0) {
+//                     q.push(NULL);
+//                 }
+//             }
+//             else{
+//                 sum=sum+first->val;
+//                 x++;
+//                 q.pop();
+//                 if(first->right){q.push(first->right);}
+//                 if(first->left){q.push(first->left);}
+//             }
+//         }
+//     return result;
+//     }
+// };
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<double>result;
-        if (!root) return result;
-        queue<TreeNode*>q;
-   q.push(root);
-   q.push(NULL);
-   double sum=0,x=0;
-        while(!q.empty())
-        {  TreeNode* first = q.front();
-            if(first==NULL)
-            { q.pop();
-              result.push_back(sum/x);
-              x=0;sum=0; 
-           if (q.size() > 0) {
-                    q.push(NULL);
+        vector<double> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        // while there are nodes remaining to be visited
+        while(!q.empty()){
+            double temp = q.size();
+            double sum = 0;
+            // iterating each level of binary tree
+            for(int i = 0; i < temp; i++){
+                auto tt = q.front();
+                q.pop();
+                sum += (tt->val);
+				// check for not NULL and pushing into queue.
+                if(tt->left){
+                    q.push(tt->left);
+                }
+                if(tt->right){
+                    q.push(tt->right);
                 }
             }
-            else{
-                sum=sum+first->val;
-                x++;
-                q.pop();
-                if(first->right){q.push(first->right);}
-                if(first->left){q.push(first->left);}
-            }
+            ans.push_back(sum/ temp);
         }
-    return result;
+        return ans;
     }
 };
