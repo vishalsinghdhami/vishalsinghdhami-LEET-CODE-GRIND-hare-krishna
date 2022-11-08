6f46636bcf47f467796d542bdb986ccfee77ec81
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-11-08 13:01:12 +0530

    Time: 9 ms (23.82%), Space: 7.2 MB (8.20%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,18 +1,42 @@
+// class Solution {
+// public:
+// string makeGood(string s) {
+//    string ans;
+//    for(int i = 0 ; i < s.size() ;i ++)
+//    {
+//      ans.push_back(s[i]);
+//      while(ans.size() && abs(ans.back()-s[i+1])== 32)
+//      {
+//      ans.pop_back();
+//      i ++;
+//      }
+//    }
+// return ans;
+
+//   }
+// };
 class Solution {
 public:
 string makeGood(string s) {
-string ans;
-for(int i = 0 ; i < s.size() ;
-i ++)
-{
-ans.push_back(s[i]);
-while(ans.size() &&
-abs(ans.back()-s[i+1])== 32)
-{
-ans.pop_back();
-i ++;
-}
-}
-return ans;
-}
-};
+ stack<char> st;
+         int diff = 'a'-'A';   //** diff b/w 2 same char (upper & lower)
+        for(char c:s){
+            if(st.size()==0)st.push(c);
+               
+            else{
+               char a = st.top();
+               if((c+diff==a || a+diff==c))st.pop(); //**if upper lower case combination found
+               else
+                 st.push(c);   
+            }
+
+        }
+
+        string ans = "";
+        while(st.size()!=0){
+           ans = st.top()+ans;
+           st.pop();
+        }
+        
+        return ans;
+}};
