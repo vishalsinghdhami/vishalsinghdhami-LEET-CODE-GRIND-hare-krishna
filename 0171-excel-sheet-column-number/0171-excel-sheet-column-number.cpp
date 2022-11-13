715c58b93df4d4480f8b6656c49751f6e23b9c55
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-11-14 01:48:25 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (24.27%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,14 +1,28 @@
+// class Solution {
+// public:
+//     int titleToNumber(string str) 
+//     {
+//       // A B C D E 
+//       //   A-A*26*26*26*26*26    B-A*26*26*26*26   C-A*26*26*26 D-A*26*26  E-A*26
+//        int columnNumber=0;int n=str.size();
+//      for(int i = 0;i < n;i++){
+//             int currentColumn = str[i] - 'A' + 1;
+//             columnNumber = columnNumber * 26 + currentColumn;
+//         }
+//         return columnNumber;
+//     }
+// };
 class Solution {
-public:
-    int titleToNumber(string str) 
-    {
-      // A B C D E 
-      //   A-A*26*26*26*26*26    B-A*26*26*26*26   C-A*26*26*26 D-A*26*26  E-A*26
-       int columnNumber=0;int n=str.size();
-     for(int i = 0;i < n;i++){
-            int currentColumn = str[i] - 'A' + 1;
-            columnNumber = columnNumber * 26 + currentColumn;
+    public: int titleToNumber(string s) {
+        int ans  = 0;
+        int p  = 0;
+        for(int i = s.size() -1; i>=0; i--){
+           char c = s[i];
+           int val = c-'A' + 1;
+           ans += val*pow(26, p);
+            p++;
         }
-        return columnNumber;
+        
+        return ans;
     }
 };
