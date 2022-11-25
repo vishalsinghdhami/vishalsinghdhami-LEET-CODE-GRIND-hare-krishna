9407bf804f3e29e4448d270d00469ae4866c88e0
Vishal Singh Dhami <83884792+vishalsinghdhami@users.noreply.github.com>
2022-11-26 02:55:23 +0530

    Time: 511 ms (77.43%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,3 @@
 
-SELECT name 
-FROM customer
-WHERE referee_id <> 2 OR referee_id is NULL
+#SELECT name FROM customerWHERE referee_id <> 2 OR referee_id is NULL
+select name from customer where referee_id is null or referee_id!=2;
